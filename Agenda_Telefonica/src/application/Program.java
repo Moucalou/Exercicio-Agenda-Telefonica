26da4ea92e5d4119e0f22365f557471d51da7e11
Moucalou <moucalou@gmail.com>
2025-04-16 19:51:56 -0300

    Created switch case for the first option

--- Agenda_Telefonica/src/application/Program.java
+++ Agenda_Telefonica/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
@@ -8,7 +9,7 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String[][] list = new String[0][2];
+		ArrayList<ArrayList<String>> phoneList = new ArrayList<>();
 		
 		char answer = 'y';
 		
@@ -18,9 +19,35 @@ public class Program {
 			System.out.println("2 - Search number by contact name");
 			System.out.println("3 - Contacts list");
 			System.out.println("4 - Remove contact");
+			System.out.println("5 - Exit");
 			System.out.print("Please, select an option: ");
 			int option = sc.nextInt();
 			sc.nextLine();
+			
+			
+			switch (option) {
+			case 1:
+				ArrayList<String> contact = new ArrayList<>();
+				System.out.print("Please, enter the contact name: ");
+				String name = sc.nextLine();
+				contact.add(name);
+				System.out.print("Please, enter the phone number: ");
+				String phoneNumber = sc.nextLine();
+				contact.add(phoneNumber);
+				phoneList.add(contact);
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			}
+			
+			
+			
 		}
 		
 		sc.close();	
